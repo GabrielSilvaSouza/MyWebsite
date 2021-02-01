e21980f01f25ce4de0662936bd3e0de8fe33b78a
Gabriel <gabrieldasilvasouza24@gmail.com>
2021-02-01 09:27:28 -0300

    Django alongs html

--- MyWebsite/hello/views.py
+++ MyWebsite/hello/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello")
+    return render(request, "hello/index.html")
+
+def gabriel(request):
+    return HttpResponse("I'm still stand, yeah yeah yeah")
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+         "name": name.capitalize()
+    })
